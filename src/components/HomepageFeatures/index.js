52c0ba350d807aa4db675c969852508b17834e2c
Sh1n3zZ <DengJunHai@foxmail.com>
2023-04-15 15:47:42 +0800

    ✨ [Change] 默认页面更改为文档内内容
    ✨ [Change] 默认页面更改为文档内内容

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,37 +1,64 @@
 import React from 'react';
 import clsx from 'clsx';
-import './css/bootstrap.min.css';
-import './css/boxicons.min.css';
-import './css/wiki.css';
-import './css/venobox.css';
-import './index.style.css';
-import './css/font_954744_g42joo6uzdr.css';
-import './css/font_2334619_bg75usmacm.css';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by React',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 
 export default function HomepageFeatures() {
-    return (
-        <section>
-        <div class="kratos-overlay"><div class="filter"></div><canvas id="canvas" width="1920" height="939"></canvas></div>
-        <div class="container" >
-         <h1>
-         <a href="https://lesun.cloud/" class="mr-auto">
-         <img src="https://zzcdn.uerr.cn/cloudreve/uploads/2023/02/10/BAAJvpal_yuncuncu.png" alt="" class="img-fluid" style={{ height: "80px" }} /> 晞云云计算</a>
-         </h1>
-          <h2>保持热爱，共赴山海 .</h2>
-          <h2>
-            <a class="sns-xcx" href="/docs">
-            <i class="zm iconfont icon-link"></i>
-            查看文档</a>
-          </h2>
-          <div id="page-info" className="display:block">
-                <p id="bginfo" class="d-lg-block">Music ID <a href="https://music.163.com/#/playlist?id=8287113265" target="_blank">8287113265</a>
-                <br />
-                Design By <a href="https://shinezz.cc">Sh1n3zZ</a>
-                <a><span id="sitetime"></span></a><br />
-                Filing Mumber <a href="https://beian.miit.gov.cn/">粤ICP备2023028108号-1</a>
-                </p>
-            </div>
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
         </div>
-        </section>
-    );
+      </div>
+    </section>
+  );
 }
